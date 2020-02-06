93bff33c3944986cf1790948a12ef50f40c8c190
SimonWang9610 <55259373+SimonWang9610@users.noreply.github.com>
2020-02-05 21:02:37 -0500

    add RegisterForm

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
 # templates of 'auth should be in 'templates/auth/'
-auth = Blueprint('auth', __name__, template_folder='/auth')
+auth = Blueprint('auth', __name__, template_folder='templates/auth')
 
 from . import views
