a9c18beb9ccadfc9e87841c71ab1f63d20460c56
SimonWang9610 <55259373+SimonWang9610@users.noreply.github.com>
2020-03-13 12:28:37 -0400

    # debug 'EditProfile' and 'EditPost'

--- app/main/views.py
+++ app/main/views.py
@@ -11,33 +11,14 @@ from .. import db
 from ..models import Role, User, Permission, Post
 from ..decorators import admin_required
 
-# @main.route('/', methods=['GET', 'POST'])
-# def index():
-#     form = NameForm()
-#
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.name.data).first()
-#         if user is None:
-#             user = User(username=form.name.data)
-#             db.session.add(user)
-#             db.session.commit()
-#             session['known'] = False
-#         else:
-#             session['known'] = True
-#
-#         session['name'] = form.name.data
-#         form.name.data = ''
-#
-#         return redirect(url_for('main.index'))
-#
-#     return render_template('index.html', form=form, name=session.get('name'),
-#                            known=session.get('known', False), current_time=datetime.utcnow())
 @main.route('/', methods=['GET', 'POST'])
 def index():
+    # create PostFrom
     form = PostForm()
     # store the content into the database
     if current_user.can(Permission.WRITE) and form.validate_on_submit():
         post = Post(body=form.body.data, author=current_user._get_current_object())
+        # store the new post into database
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
@@ -53,6 +34,10 @@ def index():
 
 @main.route('/user/<username>')
 def user(username):
+    '''
+    user page
+    :return: user.html
+    '''
     user = User.query.filter_by(username=username).first_or_404()
     # display user's posts in the profile page
     posts = user.posts.order_by(Post.timestamp.desc()).all()
@@ -61,15 +46,18 @@ def user(username):
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
+    '''
+    create EditProfileForm
+    :return: edited profile
+    '''
     form = EditProfileForm()
-
     if form.validate_on_submit():
 
         current_user.name = form.name.data
         current_user.location = form.location.data
         current_user.about_me = form.about_me.data
-
-        db.session.add(current_user.get_current_object())
+        # commit new profile in database
+        db.session.add(current_user)
         db.session.commit()
 
         flash('Your profile has been updated.')
@@ -78,7 +66,7 @@ def edit_profile():
     form.name.data = current_user.name
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
-
+    # show new profile in user's profile page
     return render_template('edit_profile.html', form=form)
 
 @main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
@@ -116,13 +104,22 @@ def edit_profile_admin(id):
 
 @main.route('/post/<int:id>')
 def post(id):
+    '''
+    show posts
+    :return: posts
+    '''
     post = Post.query.get_or_404(id)
     return render_template('post.html', posts=[post])
 
 @main.route('/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit(id):
+    '''
+    for edit old posts
+    :return: edited posts
+    '''
     post = Post.query.get_or_404(id)
+    # only author and administrators can access old posts
     if current_user != post.author and not current_user.can(Permission.ADMIN):
         os.abort(403)
     form = PostForm()
