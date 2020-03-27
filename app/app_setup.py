1f20fe2a06543b683c3d3744ec5cef0e18c03cba
SimonWang9610 <55259373+SimonWang9610@users.noreply.github.com>
2020-03-27 13:26:02 -0400

    # add production error log

--- app/app_setup.py
+++ app/app_setup.py
@@ -1,20 +1,59 @@
 import os
+import sys
+import click
+# only for testing
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 from app import create_app, db
-from app.models import User, Role
-from flask_migrate import Migrate
+from app.models import User, Role, Follow, Permission, Post, Comment
+from flask_migrate import Migrate, upgrade
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+
+@app.cli.command
+def deploy():
+    '''Run deployment tasks'''
+    # migrate the database to the latest version
+    upgrade()
+
+    #create or update user roles
+    Role.insert_roles()
+
+    # ensure all users are following themselves
+    User.add_self_follow()
+
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
+
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default=False, help='Run tests under code coverage')
+def test(coverage):
     '''
-    Run the unit test
+    Run the unit tests
     '''
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] +  sys.argv)
+
     import unittest
-    tests = unittest.TestLoader().discover(('test'))
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(test)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version:file://%s/index/html' % covdir)
+        COV.erase()
--- config.py
+++ config.py
@@ -34,9 +34,40 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABSE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # email errors to the administrators
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr= cls.FLASKY_MAIL_SENDER,
+            toaddrs=[cls.FLASKY_ADMIN],
+            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + 'Application Error',
+            credentials=credentials,
+            secure=secure
+        )
+        # only server problems are going to be emailed once the application is deployed
+        # other errors are logged to a file
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
     'default': DevelopmentConfig
 }
+
